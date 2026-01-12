79c9ba9a441356178a3b39b8fb18edf2102d2bb6
Melanie Akinyi <kleinmelanie04@gmail.com>
2026-01-12 11:35:30 +0300

    migrations

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv()
+
+DATABASE_URL = "mysql+pymysql://patient_user:strongpassword@localhost/patient_db"
+
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
