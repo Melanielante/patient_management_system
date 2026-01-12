5ef6e66f565bfe3d278b74c9ea65427c1df178ee
Melanie Akinyi <kleinmelanie04@gmail.com>
2026-01-12 12:29:38 +0300

    patient table

--- app/db/database.py
+++ app/db/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from app.db.base import Base
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://patient_user:strongpassword@localhost/patient_db"
+)
 
-DATABASE_URL = "mysql+pymysql://patient_user:strongpassword@localhost/patient_db"
-
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-Base = declarative_base()
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    echo=True,
+)
--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,15 @@
-from .database import SessionLocal
+from sqlalchemy.orm import sessionmaker
+from app.db.database import engine
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()    
+        db.close()
